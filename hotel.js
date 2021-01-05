121052f79bc5d1874ba3143667e3a45a4195c168
Seera Kaenkaew <jay@sennalabs.com>
2021-01-05 16:15:45 +0700

    test(aqoda): สร้างโรงแรมโดยกำหนดจำนวนชั้นและจำนวนห้อง

--- hotel.js
+++ hotel.js
@@ -1,7 +1,25 @@
 class Hotel {
-  constructor(floor, roomSize) {
+  constructor(floor = 0, roomPerFloor = 0) {
     this.floor = floor;
-    this.roomSize = roomSize;
+    this.roomPerFloor = roomPerFloor;
     this.rooms = [];
   }
+
+  createHotel() {
+    this.rooms = Array.from({ length: this.floor }).map((_, i) =>
+      Array.from({ length: this.roomPerFloor }).map((_, i) => {
+        let roomNumber = i + 1 < 10 ? "0" + i + 1 : i + 1;
+        return {
+          roomNumber,
+          status: null,
+          user: {
+            name: null,
+            age: null
+          }
+        };
+      })
+    );
+  }
 }
+
+module.exports = Hotel;
