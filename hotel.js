dea3593db404729c7870e2d1cfe2671f11a94faa
aurniwat <james@sennalabs.com>
2021-01-05 18:25:51 +0700

    list guest in room

--- hotel.js
+++ hotel.js
@@ -1,59 +1,64 @@
 class Hotel {
   constructor(floor = 0, roomPerFloor = 0) {
-    this.floor = floor;
-    this.roomPerFloor = roomPerFloor;
-    this.rooms = [];
-    this.keycards = [];
+    this.floor = floor
+    this.roomPerFloor = roomPerFloor
+    this.rooms = []
+    this.keycards = []
   }
 
   get rooms() {
-    return this._rooms;
+    return this._rooms
   }
 
   set rooms(value) {
-    this._rooms = value;
+    this._rooms = value
   }
 
   createHotel() {
-    this.rooms = Array.from({length: this.floor}).map((_, i) =>
-        Array.from({length: this.roomPerFloor})
-            .map((_, i) => i + 1)
-            .map(i => {
-              let roomNumber = i < 10 ? "0" + i : i;
-              return {
-                roomNumber,
-                status: null,
-                user: {
-                  name: "",
-                  age: null
-                }
-              };
-            })
-            .reduce((all, room) => ({...all, [room.roomNumber]: room}), {})
-    );
+    this.rooms = Array.from({ length: this.floor }).map((_, i) =>
+      Array.from({ length: this.roomPerFloor })
+        .map((_, i) => i + 1)
+        .map(i => {
+          let roomNumber = i < 10 ? "0" + i : i
+          return {
+            roomNumber,
+            status: null,
+            user: {
+              name: "",
+              age: null
+            }
+          }
+        })
+        .reduce((all, room) => ({ ...all, [room.roomNumber]: room }), {})
+    )
 
     this.keycards = Array.from({
       length: this.floor * this.roomPerFloor
-    }).map((_, i) => ({key: i + 1, room: null}));
+    }).map((_, i) => ({ key: i + 1, room: null }))
   }
 
   listAvailableRooms() {
-    const availableRooms = [];
+    const availableRooms = []
     for (let runningFloor = 0; runningFloor <= this.rooms.length - 1; runningFloor++) {
-      Object.entries(this.rooms[runningFloor]).forEach((array) => {
+      Object.entries(this.rooms[runningFloor]).forEach(array => {
         if (!array[1].status) {
-
-          availableRooms.push(runningFloor + 1 + array[0]);
+          availableRooms.push(runningFloor + 1 + array[0])
         }
       })
     }
 
-    console.log(availableRooms.toString());
+    console.log(availableRooms.toString())
+  }
+
+  listGuestInRoom(roomNo) {
+    const floor = +roomNo.toString().slice(0, 1)
+    const roomNumber = roomNo.toString().slice(1)
+    console.log(this.rooms[floor - 1][roomNumber]["user"]["name"])
   }
 
   book(room, name, age) {
-    const [floor, ...number] = room.toString();
-    const roomNumber = number.join("");
+    const [floor, ...number] = room.toString()
+    const roomNumber = number.join("")
 
     if (this.rooms[floor - 1][roomNumber].status !== "checkin") {
       this.rooms[floor - 1][roomNumber] = {
@@ -63,26 +68,23 @@ class Hotel {
           age
         }
       }
-      const foundFirstAvailableKeyCard = this.keycards.findIndex((keyCard)=>
-        !keyCard.room
-      )
-      this.keycards[foundFirstAvailableKeyCard].room = room;
-      console.log(`Room ${room} is booked by ${name} with keycard number 1.`);
+      const foundFirstAvailableKeyCard = this.keycards.findIndex(keyCard => !keyCard.room)
+      this.keycards[foundFirstAvailableKeyCard].room = room
+      console.log(`Room ${room} is booked by ${name} with keycard number 1.`)
     } else {
-      console.log(`Cannot book room ${floor + roomNumber} for ${name}, The room is currently booked by ${this.rooms[floor - 1][roomNumber].user.name}.`)
+      console.log(
+        `Cannot book room ${floor + roomNumber} for ${name}, The room is currently booked by ${
+          this.rooms[floor - 1][roomNumber].user.name
+        }.`
+      )
     }
-
-
   }
 
   checkoutRoom(room, guessName) {
-    const [floor, ...number] = room.toString();
-    const roomNumber = number.join("");
-
+    const [floor, ...number] = room.toString()
+    const roomNumber = number.join("")
 
     // console.log('keycards',this.keycards)
-
-
 
     // if (guessName === this.rooms[floor - 1][roomNumber].user.name) {
     //   this.rooms[floor - 1][roomNumber] = {...this.rooms[floor - 1][roomNumber], status: null, user: null};
@@ -93,4 +95,4 @@ class Hotel {
   }
 }
 
-module.exports = Hotel;
+module.exports = Hotel
--- main.js
+++ main.js
@@ -1,47 +1,48 @@
-const fs = require("fs");
-const Hotel = require("./hotel");
+const fs = require("fs")
+const Hotel = require("./hotel")
 
 class Command {
   constructor(name, params) {
-    this.name = name;
-    this.params = params;
+    this.name = name
+    this.params = params
   }
 }
 
 function main() {
-  const filename = "input.txt";
-  const commands = getCommandsFromFileName(filename);
-  let hotel;
+  const filename = "input.txt"
+  const commands = getCommandsFromFileName(filename)
+  let hotel
 
   commands.forEach(command => {
     switch (command.name) {
       case "create_hotel":
-        const [floor, roomPerFloor] = command.params;
-        hotel = new Hotel(floor, roomPerFloor);
-        hotel.createHotel();
-        console.log(
-          `Hotel created with ${floor} floor(s), ${roomPerFloor} room(s) per floor.`
-        );
-        return;
+        const [floor, roomPerFloor] = command.params
+        hotel = new Hotel(floor, roomPerFloor)
+        hotel.createHotel()
+        console.log(`Hotel created with ${floor} floor(s), ${roomPerFloor} room(s) per floor.`)
+        return
       case "book":
-        const [room, name, age] = command.params;
-        hotel.book(room, name, age);
-        return;
+        const [room, name, age] = command.params
+        hotel.book(room, name, age)
+        return
       case "list_available_rooms":
-        hotel.listAvailableRooms();
-        return;
+        hotel.listAvailableRooms()
+        return
+      case "get_guest_in_room":
+        const [roomNo] = command.params
+        hotel.listGuestInRoom(roomNo)
       case "checkout":
-        const [roomId, guessName] = command.params;
-        hotel.checkoutRoom(roomId,guessName);
-        return;
+        const [roomId, guessName] = command.params
+        hotel.checkoutRoom(roomId, guessName)
+        return
       default:
-        return;
+        return
     }
-  });
+  })
 }
 
 function getCommandsFromFileName(fileName) {
-  const file = fs.readFileSync(fileName, "utf-8");
+  const file = fs.readFileSync(fileName, "utf-8")
 
   return file
     .split("\n")
@@ -51,12 +52,12 @@ function getCommandsFromFileName(fileName) {
         new Command(
           commandName,
           params.map(param => {
-            const parsedParam = parseInt(param, 10);
+            const parsedParam = parseInt(param, 10)
 
-            return Number.isNaN(parsedParam) ? param : parsedParam;
+            return Number.isNaN(parsedParam) ? param : parsedParam
           })
         )
-    );
+    )
 }
 
-main();
+main()
